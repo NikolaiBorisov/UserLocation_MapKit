34c52effec92409e549282af1a04127b140954e4
NIKOLAI <borisov_n_88@mail.ru>
2021-04-14 11:57:18 +0300

    Initial Commit

---
+++ User-Location Map/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  User-Location Map
+//
+//  Created by NIKOLAI BORISOV on 14.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
